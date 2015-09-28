00894f27affdb743577c5639384aa355a7dc296c
Charlotte <charlotte.messmer2@gmail.com>
2015-09-28 15:55:24 +0200

    vds<cd

--- projet/src/main/java/fr/uha/miage/projet/relation/model/Tournoi.java
+++ projet/src/main/java/fr/uha/miage/projet/relation/model/Tournoi.java
@@ -1,5 +1,32 @@
 package fr.uha.miage.projet.relation.model;
 
-public class Tournoi {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Tournoi {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String lieu;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+	
 }
