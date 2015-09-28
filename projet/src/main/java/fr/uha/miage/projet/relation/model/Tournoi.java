cdef933d416bbaf69b3c1ba74f05b49818ae5d05
Sutein <gael.kretz@gmail.com>
2015-09-28 15:55:13 +0200

    toast

--- projet/src/main/java/fr/uha/miage/projet/relation/model/Tournoi.java
+++ projet/src/main/java/fr/uha/miage/projet/relation/model/Tournoi.java
@@ -1,5 +1,11 @@
 package fr.uha.miage.projet.relation.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Tournoi {
 
+	@Id
+	@GeneratedValue
+	private int idiot;
 }
