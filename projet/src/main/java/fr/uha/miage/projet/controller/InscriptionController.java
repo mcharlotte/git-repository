c66dcde889d30c30febbd5f4fdc466c89444b083
Charlotte <charlotte.messmer2@gmail.com>
2015-11-02 11:45:35 +0100

    Ajout du controller pour l'inscription

---
+++ projet/src/main/java/fr/uha/miage/projet/controller/InscriptionController.java
@@ -0,0 +1,30 @@
+package fr.uha.miage.projet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import fr.uha.miage.projet.relation.model.Utilisateur;
+import fr.uha.miage.projet.relation.repository.UtilisateurRepository;
+
+@Controller
+public class InscriptionController {
+	
+	@Autowired
+	UtilisateurRepository utilisateur;
+	
+
+	@RequestMapping("/Inscription")
+	public String requestCreation(Model model)
+	{
+		model.addAttribute("utilisateur", new Utilisateur());
+		return "Inscription";
+	}	
+	
+	@RequestMapping(value="/Inscription", method=RequestMethod.POST)
+    public String inscription(Utilisateur u) {
+        utilisateur.save(u);
+        return "redirect:/Home";
+    }
+}
