e7633a642cdf92226dbf98c858e025950efe100b
Dovahtruite <Rook!e@192.168.0.15>
2015-11-21 22:28:48 +0100

    Implémentation de la gestion des tournois: Modification de tournois marche, suppression envoie une erreur 500, probablement lié aux liens entre tournois et réservations

--- projet/src/main/java/fr/uha/miage/projet/controller/adminController.java
+++ projet/src/main/java/fr/uha/miage/projet/controller/adminController.java
@@ -1,11 +1,17 @@
 package fr.uha.miage.projet.controller;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import fr.uha.miage.projet.relation.model.Tournoi;
 import fr.uha.miage.projet.relation.repository.ReservationRepository;
 import fr.uha.miage.projet.relation.repository.TournoiRepository;
 import fr.uha.miage.projet.relation.repository.UtilisateurRepository;
@@ -30,5 +36,63 @@ public class adminController {
     	return "adminTournois";
     }
 
+	@RequestMapping("/removeTournoi/{id}")
+    public String supprimerTournoi(@PathVariable("id") Integer idTournoi)
+    {
+    	tournoi.delete(idTournoi);
+    	return "redirect:/adminTournoi";
+    }
+	
+	@RequestMapping(value = "/modifTournoi", method = RequestMethod.GET)
+    public String modifyBookmark(@RequestParam long id, Model model)
+    {
+		Tournoi old_t = tournoi.findOne((int) id);
+    	model.addAttribute("old_tourn", old_t);
+    	
+    	return "modifTournoi";
+    }
+	
+	@RequestMapping(value = "/modifTournoi", method = RequestMethod.POST)
+    public String saveModifBookmark(@RequestParam long id, Tournoi modified_tourn, RedirectAttributes redirectAttributes)
+    {
+    	//récupère le bon favoris et met a jour ces données
+    	Tournoi beta = tournoi.findOne((int) id);
+    	beta.setLieu(modified_tourn.getLieu());
+    	beta.setPrix(modified_tourn.getPrix());
+    	beta.setType(modified_tourn.getType());
+    	beta.setNbJoueurMax(modified_tourn.getNbJoueurMax());
+    	beta.setJour(modified_tourn.getJour());
+    	beta.setMois(modified_tourn.getMois());
+    	beta.setAnnee(modified_tourn.getAnnee());
+    	beta.setMinute(modified_tourn.getMinute());
+    	beta.setHeure(modified_tourn.getHeure());
+    	//le sauvegarde
+    	tournoi.save(beta);
+    	
+    	return "redirect:/adminTournois";
+    }
+	
+	@RequestMapping(value = "/createTournoi", method = RequestMethod.GET)
+	public String createBookmar(Model model,RedirectAttributes redirectAttributes) 
+	{
+		model.addAttribute("tourn_create", new Tournoi());
+		return "createTournoi";
+	}
+	
+	@RequestMapping(value = "/createTournoi", method = RequestMethod.POST)
+	public String saveNewBookmark(Tournoi tourn_create, RedirectAttributes redirectAttributes) 
+	{
+		tournoi.save(tourn_create);
+		return "redirect:/index";
+	}
+	
+	
+	
+	@RequestMapping(value="/adminUser", method=RequestMethod.GET)
+    public String listeUser(Model model) {
+		
+		model.addAttribute("utilisateur", utilisateur.findAll());
+    	return "adminUser";
+    }
     
 }
