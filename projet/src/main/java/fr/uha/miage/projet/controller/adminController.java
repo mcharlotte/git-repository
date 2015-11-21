f09f5af2e8421f1721dc60fcb6561b7e4df241ce
Dovahtruite <Rook!e@192.168.0.15>
2015-11-21 18:04:21 +0100

    Ajout de la page adminTournois.html ainsi que du controller adminController destiné à gérer les pages admin

---
+++ projet/src/main/java/fr/uha/miage/projet/controller/adminController.java
@@ -0,0 +1,34 @@
+package fr.uha.miage.projet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.uha.miage.projet.relation.repository.ReservationRepository;
+import fr.uha.miage.projet.relation.repository.TournoiRepository;
+import fr.uha.miage.projet.relation.repository.UtilisateurRepository;
+
+@Controller
+public class adminController {
+
+	@Autowired
+	UtilisateurRepository utilisateur;
+	
+	@Autowired
+	TournoiRepository tournoi;
+	
+	@Autowired
+	ReservationRepository reservation;
+	
+	
+	@RequestMapping(value="/adminTournois", method=RequestMethod.GET)
+    public String listeTournois(Model model) {
+		
+		model.addAttribute("tournoi", tournoi.findAll());
+    	return "adminTournois";
+    }
+
+    
+}
