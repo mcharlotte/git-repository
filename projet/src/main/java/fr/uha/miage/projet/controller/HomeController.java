c0e2a7c76ecf68fb1f056ff4ff5ecbfc379f5480
Charlotte <charlotte.messmer2@gmail.com>
2015-11-22 07:23:02 +0100

    nada

--- projet/src/main/java/fr/uha/miage/projet/controller/HomeController.java
+++ projet/src/main/java/fr/uha/miage/projet/controller/HomeController.java
@@ -1,20 +1,10 @@
 package fr.uha.miage.projet.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import fr.uha.miage.projet.relation.model.Reservation;
-import fr.uha.miage.projet.relation.model.Tournoi;
-import fr.uha.miage.projet.relation.model.Utilisateur;
 import fr.uha.miage.projet.relation.repository.ReservationRepository;
 import fr.uha.miage.projet.relation.repository.TournoiRepository;
 import fr.uha.miage.projet.relation.repository.UtilisateurRepository;
@@ -33,14 +23,7 @@ public class HomeController {
 	
 	
 	@RequestMapping(value="/Home", method=RequestMethod.GET)
-    public String listeTournoisAvantCo(Model model,HttpSession session) {
-		
-		model.addAttribute("tournoi", tournoi.findAll());
-    	return "Home";
-    }
-	
-	@RequestMapping(value="/Home", method=RequestMethod.POST)
-    public String listeTournoisApresCo(Model model,HttpSession session) {
+    public String listeTournoisAvantCo(Model model) {
 		
 		model.addAttribute("tournoi", tournoi.findAll());
     	return "Home";
