6ee2278ebfca5cae3d7a045009563510d242a580
Charlotte <charlotte.messmer2@gmail.com>
2015-11-22 16:22:35 +0100

    ajout rejoindre/quitter partie

--- projet/src/main/java/fr/uha/miage/projet/controller/HomeUtilisateurController.java
+++ projet/src/main/java/fr/uha/miage/projet/controller/HomeUtilisateurController.java
@@ -1,12 +1,18 @@
 package fr.uha.miage.projet.controller;
 
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fr.uha.miage.projet.relation.model.Reservation;
+import fr.uha.miage.projet.relation.model.Tournoi;
 import fr.uha.miage.projet.relation.model.Utilisateur;
+import fr.uha.miage.projet.relation.repository.ReservationRepository;
 import fr.uha.miage.projet.relation.repository.TournoiRepository;
 import fr.uha.miage.projet.relation.repository.UtilisateurRepository;
 
@@ -19,12 +25,37 @@ public class HomeUtilisateurController {
 	@Autowired
 	TournoiRepository tournoi;
 	
+	@Autowired
+	ReservationRepository reservation;
+	
 	@RequestMapping(value="/HomeUtilisateur", method=RequestMethod.GET)
 	public String AffichageFormulaire(Utilisateur u, Model model) {
+		
+		String inOut = "";
 		
 		u =utilisateur.findOne(HomeController.idUtilisateur);
 		model.addAttribute("tournoi", tournoi.findAll());
 		model.addAttribute(u);
+		
+		List<Reservation> resaU = u.getReservations();
+		for(Tournoi t : tournoi.findAll())
+		{
+			for(int i=0;i<resaU.size();i++)
+			{
+				if(t.getReservations().contains(resaU.get(i)))
+				{
+					resaU.remove(resaU.get(i));
+					inOut="Quitter";				
+				}
+				else
+				{
+					inOut="Rejoindre";
+				}
+				
+			}
+			model.addAttribute("inout", inOut);	
+		}
+			
 		return "/HomeUtilisateur";
 
     }
